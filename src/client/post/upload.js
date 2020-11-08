d9aba04297179f1776ebc77a68ab163d14720677
william-murphy <36784148+william-murphy@users.noreply.github.com>
2020-11-08 13:10:36 -0500

    post page added

---
+++ src/client/post/upload.js
@@ -0,0 +1,26 @@
+document.getElementById("publish").addEventListener("click", async function() {
+
+	//store recipe data, TODO: image upload in NodeJS?
+	const userName = "willmurphy23"; //example dummy userName
+	const recipeTitle = document.getElementById("title").value;
+	const recipeDescription = document.getElementById("description").value;
+
+	const response = await fetch("/", {
+       method: 'POST',
+       headers: {
+         'Content-Type': 'application/json;charset=utf-8'
+       },
+       body: JSON.stringify({
+		  user: userName,
+		  title: recipeTitle,
+		  desc: recipeDescription
+	  })
+     });
+     if (!response.ok) {
+       console.log(response.error);
+       return;
+  }else {
+	  alert("Successfully uploaded new recipe!");
+	  
+  }
+})
