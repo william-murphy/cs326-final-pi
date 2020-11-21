1b8956d6c1a57bf2d0c50999956272e08261975f
jye6013 <jye6013@bths.edu>
2020-11-20 20:20:12 -0500

    add local user

--- src/server/passport.js
+++ src/server/passport.js
@@ -120,7 +120,7 @@ async function validatePassword(name, pwd) {
    } else {
 	   data = await response.json();
 	   if (mc.check(pwd, data.salt, data.hash)) {
-			storage.setItem('user', JSON.stringify(loginData.name));
+			storage.setItem('user', JSON.stringify(name));
 		   return true;
 	   } else {
 		   return false;
