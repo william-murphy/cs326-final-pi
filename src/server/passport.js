a0edc042a000eebbf4620acd32bdbf6631e0a0a9
Dhruvi Chauhan <dchauhan@umass.edu>
2020-11-25 21:24:09 -0500

    more bugs

--- src/server/passport.js
+++ src/server/passport.js
@@ -79,7 +79,8 @@ async function addUser(name, email, pwd, bio, profile_pic) {
 	const encrypted = mc.hash(pwd);
 	const salt = encrypted[0];
 	const hash = encrypted[1];
-	// console.log("add_name: " + name);
+	console.log("salt: " + salt + "hash: " + hash);
+	console.log("add_name: " + name);
 	await db.signup(name, email, salt, hash, bio, profile_pic);
     return true;
 }
